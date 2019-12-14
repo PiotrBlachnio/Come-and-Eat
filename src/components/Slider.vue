98207c087ec4d5e632c769a6f176d72c682d1b0f
PiotrBlachnio <piotr.blachnio@yahoo.com>
2019-12-14 22:32:17 +0100

    Create slider component with image

---
+++ src/components/Slider.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id='slider'>
+        <b-container>
+            <b-row class='title'>
+                Select<br></br>Delicious<br></br>Try<br></br>Special.
+            </b-row>
+            <b-row>             
+            </b-row>
+        </b-container>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: 'Slider',
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Rubik&display=swap');
+   #slider {
+       height: 850px;
+       background-image: url('../assets/Image_1.jpg');
+       background-repeat: no-repeat;
+
+   }
+
+   .title {
+       color: #25292d;
+        font-family: "Rubik", Sans-serif;
+        font-size: 100px;
+        font-weight: 300;
+        text-transform: uppercase;
+        line-height: 90px;
+        padding-top: 100px;
+   }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Nav v-bind:current='current' />
+    <Slider />
   </div>
 </template>
 
 <script>
 import Nav from '../components/Nav.vue';
+import Slider from '../components/Slider.vue';
 
 export default {
   name: 'home',
   components: {
     Nav,
+    Slider
   },
   data() {
     return {
