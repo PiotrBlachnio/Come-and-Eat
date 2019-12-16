aec9b57a94e882e3810f5a5c93c7573d80f85e8a
PiotrBlachnio <piotr.blachnio@yahoo.com>
2019-12-16 07:57:28 +0100

    Add animations to slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,15 +1,15 @@
 <template>
     <div id='slider' :style="{ backgroundImage: image}">
         <div class='container-fluid'>
-            <div class="row justify-content-center">
-                <div class="col-9 title">
-                    <span class='title-text'>
-                        {{title[0]}}<br><br>
-                        {{title[1]}}<br><br>
-                        {{title[2]}}<br><br>
-                        {{title[3]}}
-                    </span>
-                </div>
+            <div class="row justify-content-center">               
+            <div class="col-9 title">
+                <span class='title-text'>
+                    {{title[0]}}<br><br>
+                    {{title[1]}}<br><br>
+                    {{title[2]}}<br><br>
+                    {{title[3]}}
+                </span>
+            </div>            
             </div>
             <div class="row justify-content-center mt-4">
                 <div class="col-9">
@@ -42,7 +42,7 @@ export default {
                 break;
             case 2:         
                 this.image = "url('./background2.jpg')";
-                this.title = ['Don\'t', 'eat less.', 'Just', 'eat real.'];     
+                this.title = ['Don\'t', 'eat less.', 'Just', 'eat real.'];              
                 break;
             case 3:         
                 this.image = "url('./background3.jpg')";
@@ -53,23 +53,35 @@ export default {
                 this.title = ['European', 'Cuisine', 'World', 'Tastes.'];     
                 break;
         }
+        document.querySelector('#slider').classList.remove('animated');
+        document.querySelector('#slider').classList.remove('fadeIn');
+        void document.querySelector('#slider').offsetWidth;
+        document.querySelector('#slider').classList.add('animated');
+        document.querySelector('#slider').classList.add('fadeIn');
     }
   }
 };
-/*
-    TODO: Rem units
-    TODO: Animations
-    TODO: Reponsive navigation
-*/
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
 @import url('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css');
+
+    @keyframes fade {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+
    #slider {
        height: 850px;
        background-repeat: no-repeat;
        background-size: contain;
+       overflow: hidden;
    }
 
    .title {       
@@ -97,6 +109,7 @@ export default {
         font-weight: 500;
         letter-spacing: 0.5px;
         margin-top: 30px;
+        transition: background-color 0.3s; 
     }
 
     .slider-button:hover {
@@ -115,9 +128,18 @@ export default {
         float: left;
         padding: 7px;
         margin: 3px;
+        outline: none;
+        transition: background-color 0.3s;
     }
 
     .active {
         background-color: #e44249;
     }
+
+    .fade {
+        animation-name: fade;
+        animation-duration: 2s;
+        animation-fill-mode: forwards;
+    }
+
 </style>
