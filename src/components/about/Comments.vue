a0a2306ed328050e2486d892a21df68dc55ab6a7
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-01-01 22:00:02 +0100

    Add arrows to slider

--- src/components/about/Comments.vue
+++ src/components/about/Comments.vue
@@ -1,18 +1,20 @@
 <template>
     <div id='comments'>
-        <div class="container-fluid" style='margin-top: 90px;'>
-            <div class="row justify-content-center">
+        <div class="container" style='margin-top: 90px;'>
+            <div class="row">
                 <div class="glide">
                     <div class="glide__track" data-glide-el="track">
                         <ul class="glide__slides">
-                            <li class="glide__slide">0</li>
-                            <li class="glide__slide">1</li>
-                            <li class="glide__slide">2</li>
+                            <li class="glide__slide">Slide 1</li>
+                            <li class="glide__slide">Slide 2</li>
+                            <li class="glide__slide">Slide 3</li>
                         </ul>
                     </div>
                     <div class="glide__arrows" data-glide-el="controls">
-                        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
-                        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+                        <font-awesome-icon icon="arrow-left" class='icon icon-left' data-glide-dir="<"/>
+                        <font-awesome-icon icon="arrow-right" class='icon icon-right' data-glide-dir=">"/>
+                        <!-- <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button> -->
+                        <!-- <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button> -->
                     </div>
                 </div>
             </div>
@@ -32,4 +34,30 @@ export default {
 <style scoped>
 @import url('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
 
+    .glide__track {
+        text-align: center;
+    }
+
+    .icon {
+        position: absolute;
+        display: block;
+        top: 50%;
+        z-index: 2;
+        cursor: pointer;
+        transform: translateY(-50%);
+    }
+
+    .icon-left {
+        left: 2em;
+    }
+
+    .icon-right {
+        right: 2em;
+    }
+
+    .glide--dragging {
+        cursor: grabbing;
+        cursor: -moz-grabbing;
+        cursor: -webkit-grabbing
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faUsers, faHamburger, faClock} from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faUsers, faHamburger, faClock, faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCoffee, faUsers, faHamburger, faClock);
+library.add(faCoffee, faUsers, faHamburger, faClock, faArrowLeft, faArrowRight);
 Vue.use(BootstrapVue);
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false;
@@ -18,6 +18,10 @@ new Vue({
   router,
   render: h => h(App),
   mounted: function() {
-    new Glide('.glide').mount()
+    const config = {
+      type: 'carousel',
+      perView: 1
+    };
+    new Glide('.glide', config).mount()
   },
 }).$mount('#app');
