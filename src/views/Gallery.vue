19ee528e5c7444e8bd0bcb4aa0f86239d2078a62
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-01-04 15:53:41 +0100

    Create gallery

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
     <Nav v-bind:current='current' />
-    <Footer v-bind:current='current'/>
+    <Photos />
+    <!-- <Footer v-bind:current='current'/> -->
   </div>
 </template>
 
 <script>
 import Nav from '../components/Nav.vue';
 import Footer from '../components/Footer.vue';
+import Photos from '../components/gallery/Photos.vue';
 
 export default {
   name: 'menu',
   components: {
     Nav,
     Footer,
+    Photos,
   },
   data() {
     return {
