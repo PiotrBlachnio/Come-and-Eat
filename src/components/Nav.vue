5498033c53fdaa6b22e6b48915ad43b4f2930f3f
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-01-04 22:44:43 +0100

    Fix navigation

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,26 +1,27 @@
 <template>
     <div class='main'>
-        <nav class='navbar navbar-expand-sm nav' >
+        <nav class='navbar navbar-expand-md nav' >
             <router-link to='home'>
-                <img width=270px height=52px src="https://ld-wp.template-help.com/rockthemes/21791/wp-content/uploads/2018/06/logo-1.png" class='logo'></img>
+                <img width=270px height=52px src="https://ld-wp.template-help.com/rockthemes/21791/wp-content/uploads/2018/06/logo-1.png" class='logo ml-auto'/>
             </router-link>
-            <button class='navbar-toggler' data-toggle='collapse' data-target='#navbarMenu'></button>
+            <!-- <button class='navbar-toggler button' data-toggle='collapse' data-target='#navbarMenu'></button> -->
+            <img :src="menu_icon" class='navbar-toggler button' data-toggle='collapse' data-target='#navbarMenu'/>
             <div class='collapse navbar-collapse' id='navbarMenu'>
                 <ul class='navbar-nav ml-auto mt-2'>
-                    <li class='item' :class="{'active':current=='home'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='home' class='link'>Home</router-link>         
+                    <li class='item nav-item' :class="{'active':current=='home'}" data-aos="fade-right" data-aos-duration="1000">
+                        <router-link to='home' class='link nav-link'>Home</router-link>         
                     </li>
-                    <li class='item' :class="{'active':current==='about'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='about' class='link'>About</router-link> 
+                    <li class='item nav-item' :class="{'active':current==='about'}" data-aos="fade-right" data-aos-duration="1000">
+                        <router-link to='about' class='link nav-link'>About</router-link> 
                     </li>
-                    <li class='item' :class="{'active':current==='menu'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='menu' class='link'>Menu</router-link> 
+                    <li class='item nav-item' :class="{'active':current==='menu'}" data-aos="fade-right" data-aos-duration="1000">
+                        <router-link to='menu' class='link nav-link'>Menu</router-link> 
                     </li>
-                    <li class='item' :class="{'active':current==='gallery'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='gallery' class='link'>Gallery</router-link> 
+                    <li class='item nav-item' :class="{'active':current==='gallery'}" data-aos="fade-right" data-aos-duration="1000">
+                        <router-link to='gallery' class='link nav-link'>Gallery</router-link> 
                     </li>
-                    <li class='item last-item' :class="{'active':current==='contact'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='contact' class='link'>Contact</router-link> 
+                    <li class='item last-item nav-item' :class="{'active':current==='contact'}" data-aos="fade-right" data-aos-duration="1000">
+                        <router-link to='contact' class='link nav-link'>Contact</router-link> 
                     </li>
                 </ul>
             </div>
@@ -32,6 +33,11 @@
 export default {
   name: 'Nav',
   props: ['current'],
+  data() {
+      return {
+          menu_icon: require('../assets/img/menu_icon.png'),
+      }
+  },
 };
 </script>
 
@@ -77,5 +83,14 @@ export default {
         font-family: 'Rubik', sans-serif;
         font-weight: 400;
         text-transform: uppercase;
+    }
+
+    .button {
+        /* height: 80%;
+        width: 80%; */
+    }
+
+    .button:hover {
+        cursor: pointer;
     }
 </style>
