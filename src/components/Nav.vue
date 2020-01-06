1218f397fc563fd77a850393292eb1894e5076f0
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-01-06 13:48:19 +0100

    Remove bootstrap navigation

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,31 +1,27 @@
 <template>
     <div class='main'>
-        <nav class='navbar navbar-expand-md nav' >
+        <div class='nav'>
             <router-link to='home'>
                 <img width=270px height=52px src="https://ld-wp.template-help.com/rockthemes/21791/wp-content/uploads/2018/06/logo-1.png" class='logo ml-auto'/>
             </router-link>
-            <!-- <button class='navbar-toggler button' data-toggle='collapse' data-target='#navbarMenu'></button> -->
-            <img :src="menu_icon" class='navbar-toggler button' data-toggle='collapse' data-target='#navbarMenu'/>
-            <div class='collapse navbar-collapse' id='navbarMenu'>
-                <ul class='navbar-nav ml-auto mt-2'>
-                    <li class='item nav-item' :class="{'active':current=='home'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='home' class='link nav-link'>Home</router-link>         
-                    </li>
-                    <li class='item nav-item' :class="{'active':current==='about'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='about' class='link nav-link'>About</router-link> 
-                    </li>
-                    <li class='item nav-item' :class="{'active':current==='menu'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='menu' class='link nav-link'>Menu</router-link> 
-                    </li>
-                    <li class='item nav-item' :class="{'active':current==='gallery'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='gallery' class='link nav-link'>Gallery</router-link> 
-                    </li>
-                    <li class='item last-item nav-item' :class="{'active':current==='contact'}" data-aos="fade-right" data-aos-duration="1000">
-                        <router-link to='contact' class='link nav-link'>Contact</router-link> 
-                    </li>
-                </ul>
-            </div>
-        </nav>      
+            <ul class='navbar ml-auto'>
+                <li class='item nav-item' :class="{'active':current=='home'}" data-aos="fade-right" data-aos-duration="1000">
+                    <router-link to='home' class='link nav-link'>Home</router-link>         
+                </li>
+                <li class='item nav-item' :class="{'active':current==='about'}" data-aos="fade-right" data-aos-duration="1000">
+                    <router-link to='about' class='link nav-link'>About</router-link> 
+                </li>
+                <li class='item nav-item' :class="{'active':current==='menu'}" data-aos="fade-right" data-aos-duration="1000">
+                    <router-link to='menu' class='link nav-link'>Menu</router-link> 
+                </li>
+                <li class='item nav-item' :class="{'active':current==='gallery'}" data-aos="fade-right" data-aos-duration="1000">
+                    <router-link to='gallery' class='link nav-link'>Gallery</router-link> 
+                </li>
+                <li class='item nav-item' :class="{'active':current==='contact'}" data-aos="fade-right" data-aos-duration="1000">
+                    <router-link to='contact' class='link nav-link'>Contact</router-link> 
+                </li>
+            </ul>
+        </div>      
     </div> 
 </template>
 
@@ -48,24 +44,25 @@ export default {
         height: 80px;
     }
 
+    .nav ul li{
+        float: left;
+        margin-top: auto;
+        list-style: none;
+    }
     .item {
-        margin-left: 20px;
-        line-height: 1.4em;
+        margin-right: 10px;
+        line-height: 1em;
         transition: all .7s;
     }
 
     .nav-link:hover {
-        cursor: default;
+        cursor: pointer;
     }
 
     .item:hover, .active {
         border-bottom: 2px solid #e44249;
         text-decoration: none;
         cursor: pointer;
-    }
-
-    .last-item {
-        margin-right: 220px;
     }
 
     .logo {
@@ -85,12 +82,8 @@ export default {
         text-transform: uppercase;
     }
 
-    .button {
-        /* height: 80%;
-        width: 80%; */
-    }
-
     .button:hover {
         cursor: pointer;
     }
+
 </style>
